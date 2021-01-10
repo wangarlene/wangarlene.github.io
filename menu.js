0c355a7af435da878724501b65430ba8c0bbad73
wangarlene <wangarlene@users.noreply.github.com>
2021-01-10 17:37:31 -0500

    responsive menu draft
    - code edit only on contact page for testing
    - added responsiveness for navbar on small devices

--- menu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenu() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
